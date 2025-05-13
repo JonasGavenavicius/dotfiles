9e95d28e26439ebaca5534c13f3f0fc1455d8eca
Jonas Gavėnavičius <jonas.gavenavicius@vinted.com>
2025-05-13 09:43:24 +0300

    binding.pry working, relative path added

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -24,80 +24,74 @@ local M = {
 
     dap.adapters.ruby = {
       type = "executable",
-      command = "rdebug-ide",
-      args = { "--host", "127.0.0.1", "--port", "1234" }
+      command = "readapt",
+      args = { "stdio" },
     }
 
     dap.configurations.ruby = {
       {
         type = "ruby",
-        name = "Debug current RSpec file",
+        name = "Debug RSpec current file",
         request = "launch",
         program = "${file}",
-        cwd = "${workspaceFolder}",
         useBundler = true,
-        command = "rspec",
-      },
-      {
-        type = "ruby",
-        name = "Debug ruby file",
-        request = "launch",
-        program = "${file}",
-        cwd = "${workspaceFolder}",
       },
     }
 
-        dap.adapters.go = {
-            type = "executable",
-            command = "node",
-            args = { "go-debug-adapter" },
-        }
-        dap.configurations.go = {
-            {
-                type = "go",
-                name = "Debug",
-                request = "launch",
-                showLog = true,
-                program = "${file}",
-                dlvToolPath = vim.fn.exepath "dlv",
-            },
-        } 
-        dap.adapters.codelldb = {
-            type = "server",
-            port = "${port}",
-            executable = {
-              command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
-              args = { "--port", "${port}" },
-            }
-          }
-        dap.configurations.rust = {
-            {
-              name = "Debug Rust",
-              type = "codelldb",
-              request = "launch",
-              program = function()
-                -- Ask user for path to binary
-                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
-              end,
-              cwd = '${workspaceFolder}',
-              stopOnEntry = false,
-            },
-          }
+    dap.adapters.go = {
+      type = "executable",
+      command = "node",
+      args = { "go-debug-adapter" },
+    }
+
+    dap.configurations.go = {
+      {
+        type = "go",
+        name = "Debug",
+        request = "launch",
+        showLog = true,
+        program = "${file}",
+        dlvToolPath = vim.fn.exepath "dlv",
+      },
+    }
+
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+        args = { "--port", "${port}" },
+      }
+    }
+
+    dap.configurations.rust = {
+      {
+        name = "Debug Rust",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          -- Ask user for path to binary
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+      },
+    }
 
 
-        local map = vim.keymap.set
-        map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
-        map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
-        map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
-        map("n", "<Leader>dc>", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
-        map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
-        map("n", "<Leader>dd", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", { desc = "Debugger set conditional breakpoint" })
-        map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
-        map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+    local map = vim.keymap.set
+    map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+    map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+    map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+    map("n", "<Leader>dc>", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+    map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+    map("n", "<Leader>dd", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", { desc = "Debugger set conditional breakpoint" })
+    map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+    map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
 
-        require("dap-go").setup()
-        require("nvim-dap-virtual-text").setup({})
-    end,
+    require("dap-go").setup()
+    require("nvim-dap-virtual-text").setup({})
+  end,
 }
 
 M.keys = function()
