7fb5ba645345b8ec6d46922a1830b4b3a701b73b
Jonas Gavėnavičius <jonas.gavenavicius@vinted.com>
2025-05-22 16:38:14 +0300

    use better gitlinker

--- lua/configs/gitlinker.lua
+++ lua/configs/gitlinker.lua
@@ -1,27 +1,14 @@
 return {
   "linrongbin16/gitlinker.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "telescope.nvim",
+  },
   config = function()
     require("gitlinker").setup()
-
-    vim.keymap.set("n", "<leader>gy", function()
-      require("gitlinker").get_buf_range_url("n")
-    end, { desc = "GitHub link to line (copy)" })
-
-    vim.keymap.set("v", "<leader>gy", function()
-      require("gitlinker").get_buf_range_url("v")
-    end, { desc = "GitHub link to selection (copy)" })
-
-    vim.keymap.set("n", "<leader>gb", function()
-      require("gitlinker").get_buf_range_url("n", {
-        action_callback = require("gitlinker.actions").open_in_browser,
-      })
-    end, { desc = "GitHub line open in browser" })
-
-    vim.keymap.set("v", "<leader>gb", function()
-      require("gitlinker").get_buf_range_url("v", {
-        action_callback = require("gitlinker.actions").open_in_browser,
-      })
-    end, { desc = "GitHub selection open in browser" })
   end,
+  keys = {
+    { "<leader>gy", "<cmd>GitLink<CR>", desc = "Copy GitHub link to current line" },
+    { "<leader>gb", "<cmd>GitLink!<CR>", desc = "Open GitHub link in browser" },
+  },
 }
