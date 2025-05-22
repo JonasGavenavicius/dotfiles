96693b6eaaf3d36154e3115233e473c1f9b5b796
Jonas Gavėnavičius <jonas.gavenavicius@vinted.com>
2025-05-22 11:22:49 +0300

    use better gitlinker

--- lua/configs/gitlinker.lua
+++ lua/configs/gitlinker.lua
@@ -1,15 +1,27 @@
 return {
-  "ruifm/gitlinker.nvim",
-  dependencies = "nvim-lua/plenary.nvim",
+  "linrongbin16/gitlinker.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("gitlinker").setup({
-      mappings = nil, -- we'll set our own below
-    })
+    require("gitlinker").setup()
 
-    vim.keymap.set("n", "<leader>gl", function()
-      require("gitlinker").get_buf_range_url("n", { action_callback = require("gitlinker.actions").copy_to_clipboard })
-    end, { desc = "Copy GitHub URL to clipboard" })
+    vim.keymap.set("n", "<leader>gy", function()
+      require("gitlinker").get_buf_range_url("n")
+    end, { desc = "GitHub link to line (copy)" })
+
+    vim.keymap.set("v", "<leader>gy", function()
+      require("gitlinker").get_buf_range_url("v")
+    end, { desc = "GitHub link to selection (copy)" })
+
+    vim.keymap.set("n", "<leader>gb", function()
+      require("gitlinker").get_buf_range_url("n", {
+        action_callback = require("gitlinker.actions").open_in_browser,
+      })
+    end, { desc = "GitHub line open in browser" })
+
+    vim.keymap.set("v", "<leader>gb", function()
+      require("gitlinker").get_buf_range_url("v", {
+        action_callback = require("gitlinker.actions").open_in_browser,
+      })
+    end, { desc = "GitHub selection open in browser" })
   end,
 }
-
-
