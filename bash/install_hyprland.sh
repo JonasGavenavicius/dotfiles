1d89f4c7b8cabd123a4d2ed775c237a2557b1fc4
Jonas Gavėnavičius <j.gavenavicius@gmail.com>
2025-07-27 10:39:04 +0300

    major factor

--- bash/install_hyprland.sh
+++ bash/install_hyprland.sh
@@ -3,13 +3,42 @@ source "$(dirname "$0")/utils.sh"
 
 log "Installing Hyprland and UI dependencies."
 
+sudo pacman -S ninja gcc wayland-protocols libjpeg-turbo libwebp libjxl pango cairo pkgconf cmake libglvnd wayland hyprutils hyprwayland-scanner hyprlang # Required libraries for Hyprland
+
 sudo pacman -S --noconfirm \
-  hyprland waybar wofi swaybg swayidle swaylock mako thunar \
-  polkit-kde-agent pipewire wireplumber xdg-desktop-portal-hyprland \
-  qt5-wayland qt6-wayland cinnamon-sound-server pavucontrol brightnessctl \
-  bluez bluez-utils blueman network-manager-applet thunar-archive-plugin file-roller \
-  noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
+  hyprland \ # Main compositor
+  waybar \ # Status bar
+  wofi \ # Application launcher
+  grim \ # Screenshot tool
+  slurp \ # Screen selection tool
+  hyprpaper \ # Wallpaper manager
+  hyprlock \ # Screen locker
+  hyprcursor \ # Cursor theme support
+  swaync \ # Notification daemon
+  tlp \ # Power management tool
+  syspower \ # System power management
+  sysstat \ # System performance monitoring tools
+  dolphin \ # File manager
+  qt6-wayland \ # Qt6 Wayland support
+  qt5-wayland \ # Qt5 Wayland support
+  xdg-desktop-portal-hyprland \ # Hyprland desktop portal
+  gvfs \ # Virtual filesystem support
+  network-manager-applet \ # Network management applet
+  wlogout \ # Logout menu
+  playerctl # Player controller for media keys
+
+sudo yay -S --noconfirm \
+  swayosd-git \ # On-screen display for Hyprland
+  powerstat \ # Power consumption statistics tool
+  google-chrome \ # Web browser
+  1password # Power consumption statistics tool
 
 # Enable bluetooth service
 sudo systemctl enable --now bluetooth
+sudo systemctl enable --now sysstat
+sudo systemctl enable --now tlp
+
+# Copy configuration files
+cp -r ../hyprland/hypr ~/.config/
+cp -r ../hyprland/wayland ~/.config/
 
--- bash/install_pacman.sh
+++ bash/install_pacman.sh
@@ -8,7 +8,8 @@ sudo pacman -S --noconfirm \
   curl wget git unzip base-devel \
   zsh tmux neovim fd fzf ripgrep bat \
   docker docker-compose ruby jq p7zip \
-  python-pip go dotnet-sdk rustup stylua eza nodejs yarn
+  python-pip go rustup stylua eza nodejs yarn \
+  ghostty networkmanager
 
 log "Installing yay (AUR helper)"
 git clone https://aur.archlinux.org/yay.git /tmp/yay
@@ -16,7 +17,8 @@ git clone https://aur.archlinux.org/yay.git /tmp/yay
 rm -rf /tmp/yay
 
 # Enable docker service if desired
-# sudo systemctl enable --now docker
+sudo systemctl enable --now docker
+sudo systemctl enable --now NetworkManager
 
 "$(dirname "$0")/install_hyprland.sh"
 
